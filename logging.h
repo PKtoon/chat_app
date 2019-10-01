1e791d9e065c5d5eed06cd67b0cf4e9cacd10c31
pk2n <pratik_khanke005@yahoo.com>
2019-10-01 22:33:02 +0530

    logging.h no references in parameters

--- logging.h
+++ logging.h
@@ -11,21 +11,21 @@ inline void logIt(std::string name,std::string msg/*, std::ifstream& file*/)
 //    file<<msg;
 }
 
-inline void logError(std::string name,std::string msg, const boost::system::error_code& error)
+inline void logError(std::string name,std::string msg, const boost::system::error_code error)
 {
     std::ostringstream os;
     os<<error;
     std::cerr<<name<<msg<<os.str()<<std::endl;
 }
 
-inline void logRead(std::string& name, std::string msg, unsigned long& data)
+inline void logRead(std::string name, std::string msg, unsigned long data)
 {
     std::ostringstream os;
     os<<data;
     std::cerr<<name<<msg<<os.str()<<std::endl;
 }
 
-inline void logWrite(std::string& name, std::string msg, std::string receiver, unsigned long data)
+inline void logWrite(std::string name, std::string msg, std::string receiver, unsigned long data)
 {
     std::ostringstream os;
     os<<data;
