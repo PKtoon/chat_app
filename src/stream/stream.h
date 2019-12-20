a7041d7f791be314e68d6a6033f74bd64dc00075
PK2n <pratik_khanke005@yahoo.com>
2019-12-20 21:45:33 +0530

    New Header system

--- src/stream/stream.h
+++ src/stream/stream.h
@@ -9,33 +9,54 @@
 #include <boost/serialization/string.hpp>
 #include <boost/asio.hpp>
 
-enum {MESSAGE, LOCAL_FILE};
+enum Header
+{
+    EMPTY = 0,
+    ACK = 1,
+    ERROR = 1<<1,
+    CLOSE = 1<<2,
+    INIT = 1<<3,
+    PING = 1<<4,
+    MESSAGE = 1<<5,
+    GROUP = 1<<6,
+    LOCAL_FILE = 1<<7
+};
 
 class Stream
 {
     friend class boost::serialization::access;
-    char type='m';
+public:
+    Header head = Header::EMPTY;
     std::string sender;
     std::string receiver;
     std::string data1;
+    unsigned int msgID;
+    unsigned int currentPart;
+    unsigned int totalParts;
     std::vector<char> data2;
     
     template <class Archive> void serialize(Archive& ar, const unsigned int version)
     {
-        ar & type;
-        ar & sender;
-        ar & receiver;
-        ar & data1;
-        if(type!='m')
+        ar & head;
+        switch(head)
         {
-            ar & data2;
+            case Header::INIT:
+                ar & sender;
+                break;
+            case Header::INIT | Header::ACK:
+                break;
+            default:
+                break;
         }
+//         ar & head;
+//         ar & sender;
+//         ar & receiver;
+//         ar & data1;
     }
-    
-public:
+
     Stream(){}
     Stream(std::string s) { getUnSerialized(s);}
-    Stream(std::string s, std::string r, std::string msg): sender{std::move(s)}, receiver{std::move(r)},data1{std::move(msg)}{}
+    Stream(std::string s, std::string r, std::string msg): sender{std::move(s)}, receiver{std::move(r)}, data1{std::move(msg)}{}
     std::string getSender() { return sender; }
     std::string getReceiver() { return receiver;}
     std::string getData1() { return data1; }
