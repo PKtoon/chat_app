cf82b48aa443b835f23cef769970ed0e30f07d6a
PK2n <pratik_khanke005@yahoo.com>
2020-04-26 20:26:18 +0530

    Added getConnection function.

--- src/network-interface/netface.h
+++ src/network-interface/netface.h
@@ -22,6 +22,7 @@ public:
     asio::ip::tcp::socket& getSocket() { return connMan->getSocket(); }
     void connect(std::string hostname, std::string port, std::function<void(asio::error_code)> callBack);
     void disconnect();
+    ConnectionManager* getConnection() { return connMan;}
     void newConnection(asio::io_context& io);
     void newConnection(asio::ip::tcp::socket sock);
     void send(Stream data, std::function<void(asio::error_code,std::size_t)> callBack);
