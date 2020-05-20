12d54c007456324dadc11a50b00635a9bf6b2d34
PK2n <pratik_khanke005@yahoo.com>
2020-05-20 18:35:19 +0530

    Finally client code is separate from GUI code. Hopefully any changes in client code will not affect GUI. This is initial working state commit more refinement is needed.

--- src/client-gui/mainwindow.h
+++ src/client-gui/mainwindow.h
@@ -6,13 +6,9 @@
 #include <QMainWindow>
 #include <QListWidget>
 
-#include <asio.hpp>
-
-#include "src/network-interface/netface.h"
 #include "conndialog.h"
 #include "newcontactdialog.h"
-
-#include "external/SQLite/sqlite3_wrap.h"
+#include "src/client-gui/client.hpp"
 
 class MainWindow : public QMainWindow
 {
@@ -42,14 +38,10 @@ private:
     
     //networking scaffold
     asio::io_context io;
-    NetFace net;
+    Client client;
     std::thread ioThread;
     bool isThreadRunning{false};
     asio::executor_work_guard<asio::io_context::executor_type> work = asio::make_work_guard(io);
-    
-    // I/O scaffold
-    std::deque<Stream> writeQueue;
-    bool isWriting{false};
     
     // GUI components
     ConnDialog* connDialog;
@@ -64,21 +56,14 @@ private:
     void createMenuBar();
     void decorate();
     void setContactList();
-    void processMessage(Stream data);
     QListWidgetItem* makeContact(const QString& text);
     QListWidgetItem* getUser(QString name);
     
     //client.h
     void initialize();
     void reader();
-    void writer();
-    void processData(Stream);
-    void queueMessage(Stream);
-    void ping();
-    
-    //SQLite3 db
-    SQLite3DB db {"storage"};
-    void initDB();
+    void processData(Stream data);
+    void processMessage(Stream data);
 };
 
 #endif // MAINWINDOW_H
