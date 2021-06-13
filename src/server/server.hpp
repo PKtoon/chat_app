80c474f426a3728b6ba444b22e8a75ee8f6b2ecc
PK2n <pratik_khanke005@yahoo.com>
2021-06-13 19:03:31 +0530

    Added comments to Server and User header files to remind myself what everything does.

--- src/server/server.hpp
+++ src/server/server.hpp
@@ -1,6 +1,36 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+/*
+ **********************************************************************************************************************************************************************
+ * Server serves 'Users' created by Server itself.
+ * 
+ * Author: Pratik Khanke
+ * 
+ * Tasks of Server:
+ *      Handles IOContext
+ *      Accepts incoming connections
+ *      Maintains list of active users
+ *      Maintains database of all users, pending messages, groups and group members
+ *      Delivers messages to appropriate active User object
+ *      Stores messages of inactive users
+ *      Creates and manages groups of user
+ *      Give required services to User class
+ *
+ * Required Tables in PostgreSQL database:
+ *      create table users (username text PRIMARY KEY NOT NULL, passwd text NOT NULL);
+ *      create table pending (ID integer PRIMARY KEY NOT NULL, username text NOT NULL, message text NOT NULL, timestamp TIMESTAMPTZ NOT NULL);
+ *      create table group_members (ID integer PRIMARY KEY NOT NULL, username text NOT NULL, groupname text NOT NULL);
+ *
+ *  TODO:
+ *      1. In createGroup(), usernames are separated using ';', so prevent use of that symbol in username or do something to avoid potential error in username.
+ *      2. In createGroup(), make sure every members get message of "Group Created".
+ *      3. Make Users in userList a shared_ptr. This will prevent accessing a nullptr.
+ *      4. Admin system for group.
+ **********************************************************************************************************************************************************************
+ */
+
+
 #include <vector>
 
 #include "user.hpp"
@@ -8,21 +38,21 @@
 
 class Server
 {
-    asio::io_context io;
-    asio::ip::tcp::endpoint endpoint;
-    asio::ip::tcp::acceptor acceptor;
+    asio::io_context io;                        //Executor for network operations
+    asio::ip::tcp::endpoint endpoint;           //Endpoint to bind server
+    asio::ip::tcp::acceptor acceptor;           //Accepts connections
      
-    std::vector<std::unique_ptr<User>> userList;
+    std::vector<std::unique_ptr<User>> userList;        //List of active users
     std::mutex userListMutex;
-    std::list<Stream> deliveryList;
+    std::list<Stream> deliveryList;                     //List of messages to be delievered
     std::mutex deliveryListMutex;
-    asio::steady_timer deliveryTimer {io,asio::chrono::seconds(1)};
+    asio::steady_timer deliveryTimer {io,asio::chrono::seconds(1)};     //Wait between successive delievery loop
 
-    pk::PSQLdb db;
+    pk::PSQLdb db;              //Handles database operations
 
-    void accept();
-    void deliverMessages();
-    void deliveryScheduler();
+    void accept();              //Accepts incoming connections
+    void deliverMessages();     //Loop that delivers or stores pending messages
+    void deliveryScheduler();   //Schedule execution of delivery loop after specified time
 
 public:
     Server(unsigned short port, std::string dbUri) : endpoint{asio::ip::tcp::v6(),port}, acceptor{io,endpoint}, db{dbUri}
@@ -32,20 +62,20 @@ public:
         io.run();
     }
   
-    User* getActiveUser(std::string);
-    void removeMe(User*);
-    void queueDelivery(Stream);
+    User* getActiveUser(std::string);                                   //Gives User pointer of currently active user
+    void removeMe(User*);                                               //Users call this function when they disconnects from client which removes them from active users list and destroys them
+    void queueDelivery(Stream);                                         //queues the message to deliveryList of server which later delivers it
 
-    void addUser(std::string name, std::string passwd);
-    pqxx::result getUser(std::string name);
-    bool authUser(std::string name, std::string passwd);
-    std::list<Stream> getPendingMessages(std::string name);
-    void storePendingMessage(std::string subject,Stream data);
-    void sendUserMessage(Stream data);
-    void sendGroupMessage(Stream data);
-    std::list<std::string> getGroupMembers(std::string groupName);
-    void createGroup(Stream data);
-    pqxx::result getGroup(std::string groupName);
+    void addUser(std::string name, std::string passwd);                 //User object calls this function if the client attached to it is not is in server's database that is client is new
+    pqxx::result getUser(std::string name);                             //Finds a user in database and returns the database result
+    bool authUser(std::string name, std::string passwd);                //Checks if user is present in database and compares the password. If both matched then it returns true else false
+    std::list<Stream> getPendingMessages(std::string name);             //User object calls this function to get pending messages from database
+    void storePendingMessage(std::string subject,Stream data);          //Stores messages which were unable to sent to client
+    void sendUserMessage(Stream data);                                  //Send message to a User object or stores as pending message
+    void sendGroupMessage(Stream data);                                 //Sends a message to members of a group or stores as pending message of inactive members
+    std::list<std::string> getGroupMembers(std::string groupName);      //Retrieves a list of members belonging to a particular group
+    void createGroup(Stream data);                                      //Creates a group
+    pqxx::result getGroup(std::string groupName);                       //Get name of group from database
 };
 
 #endif // SERVER_H
--- src/server/user.hpp
+++ src/server/user.hpp
@@ -1,6 +1,25 @@
 ﻿#ifndef USER_H
 #define USER_H
 
+/**************************************************************************************
+ * User handles clients.
+ *
+ * Author: Pratik Khanke
+ *
+ * Tasks of User:
+ *      Handles clients.
+ *      Handles socket.
+ *      Actually sends and receive messages.
+ *      Uses ping to check if client is connected.
+ *
+ * TODO:
+ *      Properly handle situation where client is already signed in
+ *      Group related operations
+ *
+ **************************************************************************************
+ */
+
+
 #ifndef NDEBUG
 #include <iostream>
 #endif
@@ -13,19 +32,19 @@ class Server;
 
 class User
 {
-    std::string name;
+    std::string name;       //Name of client connected
 #ifndef NDEBUG
     std::string name2;
 #endif
-    bool isAlive{false};
-    unsigned short count = 0;
-    std::list<Stream> writeQueue;
+    bool isAlive{false};                    //Tells if client is still connected
+    unsigned short count = 0;               //Retries counter
+    std::list<Stream> writeQueue;           //Messages to be sent
     std::mutex writeQueueMutex;
-    unsigned int currentQueueIndex = 0;
-    NetFace net;
-    Server& server;
-    asio::steady_timer pulseTimer {net.getSocket()->get_executor(),asio::chrono::seconds(300)};
-    asio::steady_timer writeTimer {net.getSocket()->get_executor()};
+    unsigned int currentQueueIndex = 0;     //Necessary for making circular queue
+    NetFace net;                            //Network Interface
+    Server& server;                         //Server reference to access services
+    asio::steady_timer pulseTimer {net.getSocket()->get_executor(),asio::chrono::seconds(300)};         //Wait between successive pings
+    asio::steady_timer writeTimer {net.getSocket()->get_executor()};                                    //Wait between successive write intervals
 public:
     User(asio::ip::tcp::socket socket, Server& serv);
 #ifndef NDEBUG
@@ -35,18 +54,18 @@ public:
     }
 #endif
     const std::string& getName() const { return name; }
-    void queueMessage(Stream);
+    void queueMessage(Stream);              //Queues message to send
 private:
-    void authHandler(Stream data);
-    void reader();
-    void writer();
-    void writeScheduler();
-    void processData(Stream);
-    void pingMe();
-    void checkPulse();
-    void findContact(Stream data);
-    void checkPendingMessages();
-    void findGroup(Stream data);
+    void authHandler(Stream data);          //Authenticates the client
+    void reader();                          //Receives message from client
+    void writer();                          //Sends message
+    void writeScheduler();                  //Schedules next write loop
+    void processData(Stream);               //Processes received message
+    void pingMe();                          //Pings client
+    void checkPulse();                      //Schedules next ping
+    void findContact(Stream data);          //Tells client if contact exists
+    void checkPendingMessages();            //Checks for pending messages when offline
+    void findGroup(Stream data);            //Tells client if group exists
 };
 
 #endif
