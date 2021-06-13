3d5bfd13c263ed31ef227af2f029e4347a34564b
PK2n <pratik_khanke005@yahoo.com>
2021-06-13 19:48:21 +0530

    Added comments to NetFace and ConnectionManager header files

--- include/network-interface/netface.hpp
+++ include/network-interface/netface.hpp
@@ -1,6 +1,24 @@
 #ifndef NETFACE_H
 #define NETFACE_H
 
+/**************************************************************************************
+ * NetFace is network interface and implements the protocol for communication
+ *
+ * Author: Pratik Khanke
+ *
+ * Tasks of NetFace:
+ *      Provide convenient wrapper for sending and receiving protocol
+ *      Handles socket
+ *      Handles connecting to server
+ * 
+ * TODO:
+ *      Properly handle ConnectionManager
+ *      A good structure (multiple ConnectionManager or not)
+ *      Remove magic numbers and take them as arguement
+ *
+ **************************************************************************************
+ */
+
 #include "../src/network-interface/connection-manager/connection-manager.hpp"
 #include "../src/network-interface/stream/stream.hpp"
 
@@ -8,11 +26,12 @@
 
 class NetFace
 {
-    int headerLength = 4;
+    int headerLength = 4;                                               //Header length to store size of payload
     ConnectionManager* connMan = nullptr;
     asio::steady_timer timer;
     std::deque<std::unique_ptr<ConnectionManager>> socketQueue;
     bool isTimerRunning {false};
+    
 public:
     NetFace(asio::io_context& io);
     NetFace(asio::ip::tcp::socket sock);
@@ -22,15 +41,15 @@ public:
             delete connMan;
     }
     
-    asio::ip::tcp::socket* getSocket();
-    ConnectionManager* getConnMan() { return connMan; }
-    void connect(std::string hostname, std::string port, std::function<void(asio::error_code)> callBack);
-    asio::error_code disconnect();
+    asio::ip::tcp::socket* getSocket();                                                                             //Returns pointer to socket
+    ConnectionManager* getConnMan() { return connMan; }                                                             //Returns pointer to ConnectionManager
+    void connect(std::string hostname, std::string port, std::function<void(asio::error_code)> callBack);           //Connects to given host and port
+    asio::error_code disconnect();                                                                                  //disconnects
     void removeConnMan();
     void newConnection(asio::io_context& io);
     void newConnection(asio::ip::tcp::socket sock);
-    void send(Stream data, std::function<void(asio::error_code,std::size_t)> callBack);
-    void receive(std::function<void(Stream,asio::error_code,std::size_t)> callBack);
+    void send(Stream data, std::function<void(asio::error_code,std::size_t)> callBack);                             //Sends data using protocol
+    void receive(std::function<void(Stream,asio::error_code,std::size_t)> callBack);                                //Receives data using protocol
 };
 
 #endif // NETFACE_H
--- src/network-interface/connection-manager/connection-manager.hpp
+++ src/network-interface/connection-manager/connection-manager.hpp
@@ -1,6 +1,20 @@
 #ifndef CONNECTIONMANAGER_H
 #define CONNECTIONMANAGER_H
 
+/**************************************************************************************
+ * Connection Manager wraps ASIO funtions for convenience
+ *
+ * Author: Pratik Khanke
+ *
+ * Tasks of Connection Manager:
+ *      Provide convenient wrapper for sending and receiving data
+ *      Handles socket
+ *      Handles connecting to server
+ *
+ **************************************************************************************
+ */
+
+
 #ifdef __WIN32__
 
 #include <winsock2.h>
@@ -17,24 +31,25 @@ class ConnectionManager
     asio::ip::tcp::resolver resolver;
     asio::ip::tcp::resolver::results_type endpoints;
 
-    std::vector<char> inData;
-    unsigned int inDataSize=0;
+    std::vector<char> inData;           //Input buffer
+    unsigned int inDataSize=0;          //Input size
 
 public:
     ConnectionManager(asio::io_context& io):socket{io}, resolver{io}{}
     ConnectionManager(asio::ip::tcp::socket sock) : socket{std::move(sock)}, resolver{socket.get_executor()} {}
     ~ConnectionManager(){ disconnect(); }
-    asio::ip::tcp::socket& getSocket() { return socket; }
-    void setEndpoints(std::string hostname, std::string port)
+
+    asio::ip::tcp::socket& getSocket() { return socket; }           //Returns reference to socket
+    void setEndpoints(std::string hostname, std::string port)       //Sets endpoint to connect
     {
         endpoints = resolver.resolve(/*asio::ip::tcp::v4(),*/hostname,port);
     }
     
-    asio::error_code disconnect();
-    void connector(std::function<void(asio::error_code, asio::ip::tcp::endpoint)> callBack);
-    void writer(const std::vector<char> outData, std::function<void (asio::error_code,std::size_t)> callBack);
-    void writer(const std::vector<asio::const_buffer> buffer, std::function<void (asio::error_code,std::size_t)> callBack);
-    void reader(int length, std::function<void(std::vector<char>,asio::error_code,std::size_t)> callBack);
+    asio::error_code disconnect();                                                                                                  //Disconnects
+    void connector(std::function<void(asio::error_code, asio::ip::tcp::endpoint)> callBack);                                        //Connects to endpoint
+    void writer(const std::vector<char> outData, std::function<void (asio::error_code,std::size_t)> callBack);                      //Sends data (takes vector of char)
+    void writer(const std::vector<asio::const_buffer> buffer, std::function<void (asio::error_code,std::size_t)> callBack);         //Sends data (takes vector of asio::const_buffer)
+    void reader(int length, std::function<void(std::vector<char>,asio::error_code,std::size_t)> callBack);                          //Receives data
 };
 
 #endif // CONNECTIONMANAGER_H
